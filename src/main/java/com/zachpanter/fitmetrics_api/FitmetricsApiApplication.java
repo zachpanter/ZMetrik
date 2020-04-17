30ecafe0267b16aa8f35b6b7024a1dc25799f365
Zach Panter <zachpanter@gmail.com>
2020-04-17 14:44:11 -0600

    Initial commit

---
+++ src/main/java/com/zachpanter/fitmetrics_api/FitmetricsApiApplication.java
@@ -0,0 +1,13 @@
+package com.zachpanter.fitmetrics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitmetricsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitmetricsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zachpanter/fitmetrics_api/FitmetricsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zachpanter.fitmetrics_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmetricsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
