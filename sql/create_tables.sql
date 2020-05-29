b872bfabb13759b043c98b75fa40838afea3deab
Zach Panter <zachpanter@gmail.com>
2020-05-29 11:13:30 -0600

    SQL updates, inserts for 29 May

--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,10 +1,10 @@
 CREATE TABLE lifts (
-    lift_id             INTEGER     PRIMARY KEY AUTOINCREMENT,
+    id             INTEGER     PRIMARY KEY AUTOINCREMENT,
     title               VARCHAR             NOT NULL,
     current_one_rep_max INTEGER
 );
 
-CREATE TABLE metriks(
+CREATE TABLE rauminhalt(
     id          INTEGER         PRIMARY KEY AUTOINCREMENT,
     timestamp   TIMESTAMP   DEFAULT CURRENT_TIMESTAMP,
     lift_id     INTEGER         NOT NULL,
@@ -12,5 +12,18 @@ CREATE TABLE metriks(
     weight      INTEGER         NOT NULL,
     intensity   REAL         NOT NULL,
 
-    FOREIGN KEY (lift_id) REFERENCES lift(lift_id)
+    FOREIGN KEY (lift_id) REFERENCES lifts(id)
+);
+
+CREATE TABLE morphometriks(
+    id          INTEGER         PRIMARY KEY AUTOINCREMENT,
+    timestamp   INTEGER         DEFAULT CURRENT_TIMESTAMP,
+    metrik_id   INTEGER         NOT NULL,
+
+    FOREIGN KEY (metrik_id) REFERENCES metriks(id)
+);
+
+CREATE TABLE metriks(
+    id          INTEGER         PRIMARY KEY AUTOINCREMENT,
+    title       VARCHAR         NOT NULL
 );
